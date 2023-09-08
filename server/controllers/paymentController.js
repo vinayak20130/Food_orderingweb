7277d22e11dac2d204e9f68b3ec1f86afa356c85
Vinayak Verma <78793396+vinayak20130@users.noreply.github.com>
2023-09-08 12:12:09 +0530

    Add files via upload

---
+++ server/controllers/paymentController.js
@@ -0,0 +1,58 @@
+import { instance } from "../server.js";
+import crypto from "crypto";
+import { Payment } from "../models/paymentModel.js";
+
+export const checkout = async (req, res) => {
+  const options = {
+    amount: Number(req.body.totalAmount * 100),
+    currency: "INR",
+  };
+  const order = await instance.orders.create(options);
+  res.status(200).json({
+    success: true,
+    order,
+  });
+};
+
+export const paymentVerification = async (req, res) => {
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+    req.body;
+
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_APT_SECRET)
+    .update(body.toString())
+    .digest("hex");
+
+  const isAuthentic = expectedSignature === razorpay_signature;
+
+  if (isAuthentic) {
+    try {
+      console.log('Payment data:', {
+        razorpay_order_id,
+        razorpay_payment_id,
+        razorpay_signature,
+      });
+
+      console.log('Saving payment information...');
+      const newPayment = await Payment.create({
+        razorpay_order_id,
+        razorpay_payment_id,
+        razorpay_signature,
+      });
+
+      console.log('Payment information saved successfully:', newPayment.toObject());
+    } catch (error) {
+      console.error('Error while saving payment information:', error);
+    }
+
+    res.redirect(
+      `http://localhost:3000/paymentsuccess?reference=${razorpay_payment_id}`
+    );
+  } else {
+    res.status(400).json({
+      success: false,
+    });
+  }
+};
